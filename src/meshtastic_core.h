12d4a8f91a573b171e7c79f7d99d53d3eb8e2dc0
TruongSinh Tran-Nguyen <i@truongsinh.pro>
2026-07-16 10:13:20 -0700

    core: make the spreading factor configurable
    The modem always ran LongFast (SF 11); joining a MediumFast or ShortFast
    mesh required editing the driver defaults. Add spread_factor to
    meshtastic_config (0 keeps SF 11), validate 7-12, and program it in both
    lora_config() paths. BW 250 kHz / CR 4/5 stay fixed, so SF alone selects
    the Meshtastic preset.
    
    Signed-off-by: TruongSinh Tran-Nguyen <i@truongsinh.pro>

--- src/meshtastic_core.h
+++ src/meshtastic_core.h
@@ -43,6 +43,7 @@ struct meshtastic_context {
 	uint8_t hop_limit;
 	int8_t tx_power;
 	uint32_t frequency;
+	uint8_t spread_factor;
 	const char *channel_name;
 	const char *long_name;
 	const char *short_name;
